1374ad961a8a505a18c9d8f18ca14598124371bc
haejun1213 <162427472+haejun1213@users.noreply.github.com>
2024-03-11 15:10:27 +0900

    3월11일

---
+++ chap04/src/print/Example02.java
@@ -0,0 +1,13 @@
+package print;
+
+public class Example02 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("* Java Program");
+		System.out.println("* Java\tProgram");
+		System.out.println("* Java\nProgram");
+		System.out.println("* \"Java Program\"");
+	}
+
+}
