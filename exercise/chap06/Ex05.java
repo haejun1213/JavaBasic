d393813328536e8b8006be699c7a8c1936045666
haejun1213 <162427472+haejun1213@users.noreply.github.com>
2024-05-02 11:37:08 +0900

    Update Ex05.java

--- exercise/chap06/Ex05.java
+++ exercise/chap06/Ex05.java
@@ -7,12 +7,12 @@ public class Ex05 {
 		Phone[] phones = { new Phone("황진이"), new Telephone("길동이", "내일"), new Smartphone("민국이", "갤러그")};
 		
 		for(Phone p : phones) {
-			if(p instanceof Phone) {
-				p.talk();
-			} else if (p instanceof Telephone) {
+			 if (p instanceof Smartphone){
+				((Smartphone) p).playGame();
+			} else if(p instanceof Telephone) {
 				((Telephone) p).autoAnswering();
 			} else {
-				((Smartphone) p).playGame();
+				p.talk();
 			}
 		}
 		
