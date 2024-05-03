99e581bc3a56f51133c18ff2594e7711f9558bc8
haejun1213 <162427472+haejun1213@users.noreply.github.com>
2024-05-03 16:46:49 +0900

    Add files via upload

---
+++ BookMarket/ver1.0/Person.java
@@ -0,0 +1,19 @@
+
+public abstract class Person {
+
+	String name;
+	int phone;
+	
+}
+
+class Custumer extends Person{
+	
+	String address;
+	
+}
+
+class Adim extends Person {
+	
+	String id;
+	String password;
+}
