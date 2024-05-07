1f4284a54d1d1f225ea462cab36dcac55e5ef18c
haejun1213 <162427472+haejun1213@users.noreply.github.com>
2024-05-07 11:22:28 +0900

    Add files via upload

---
+++ chap13/remoteControl/RemoteControlTest.java
@@ -0,0 +1,29 @@
+package remoteControl;
+
+public class RemoteControlTest {
+
+	public static void main(String[] args) {
+
+		RemoteControl rc;
+
+		rc = new Television();
+		rc.turnOn();
+		rc.setVolume(20);
+		rc.setMute(true);
+		rc.setMute(false);
+		rc.turnOff();
+		System.out.println();
+
+		rc = new Audio();
+		rc.turnOn();
+		rc.setVolume(5);
+		rc.setMute(true);
+		rc.setMute(false);
+		rc.turnOff();
+		System.out.println();
+		
+		RemoteControl.changeBattery();
+		
+	}
+
+}
